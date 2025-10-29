530fb10ac5a1ea7297edd69beea144683068ed26
Luis24Alejandro <luisalejandrovb@gmail.com>
2025-10-29 13:42:02 -0600

    Add initial files

---
+++ Server/Admin.java
@@ -0,0 +1,17 @@
+/*
+ * Necesario para que se puedan comunicar los documentos de Admin y de Server
+ */
+package Server;
+
+
+public class Admin {
+    public static void main(String[] args) {
+        System.out.println(args[0]);
+        System.out.println(1);
+        System.out.println(args[1]);
+        System.out.println(args[2]);
+        // hola que loco esto 
+        System.out.println(2);
+        
+    }
+}
